47364b9bca676a54aa4ffc26404d55c5c533206d
R7\hlr <hlr@live.com>
2026-04-20 13:15:52 +0800

    fix(config): ensure root and base properties are defined in vite configuration

--- dashboard-vue/vite.config.ts
+++ dashboard-vue/vite.config.ts
@@ -6,6 +6,8 @@ import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
+  root: __dirname,
+  base: '/',
   plugins: [
     vue(),
     AutoImport({
